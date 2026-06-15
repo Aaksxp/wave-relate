af44244f8f6fdc457d27e6ee1779de3d61c1338c
Aakass <aakasselango@gmail.com>
2026-06-15 11:24:22 +0530

    feat: Add migration for person category and set default value

---
+++ WaveRelate.Server/Data/Migrations/20260615044313_AddPersonCategory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WaveRelate.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPersonCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Category",
+                table: "Persons",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Persons");
+        }
+    }
+}
---
+++ WaveRelate.Server/Data/Migrations/20260615044449_SetPersonCategoryDefault.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WaveRelate.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetPersonCategoryDefault : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Category",
+                table: "Persons",
+                type: "int",
+                nullable: false,
+                defaultValue: 1,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Category",
+                table: "Persons",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 1);
+        }
+    }
+}
